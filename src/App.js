f419a9c394b17efbaa91eb8cfdd19efbec4474c2
Peter Anderson <pete.anderson@gmail.com>
2024-01-08 15:06:01 +0100

    --add: demo end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import "./styles.css";
 
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <h1>My lovely buttons</h1>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+    </>
+  );
 }
 
+// React to changes
+
+// how can we handle events in React?
+// addEventListener
+// how can we tell React to "re-draw" the page
+
+// we can use a hook to manage state.
+
+import { useState } from "react";
+
 function Counter() {
-  return <button>You have clicked this button 0 times</button>;
+  // let count = 42;
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    console.log("clicked the button (named function)");
+    // count = count + 1;
+    // count++;
+
+    setCount(count + 1);
+  }
+
+  console.log("about to render");
+  return (
+    <button
+      // onClick={() => {
+      //   console.log("clicked the button!");
+      // }}
+      onClick={handleClick}
+    >
+      You have clicked this button {count} times
+    </button>
+  );
 }
